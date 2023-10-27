8a7e01e9fa2348a4380a3eadc3031f82033f61d1
Howarf <53963484+Howarf@users.noreply.github.com>
2023-10-27 15:58:56 +0900

    Update db.js

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'xoqhd@!135',
+    password: 'Password',
     database: 'mytdl'
 });
 
